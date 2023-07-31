2751d4f5453138cf6c5facc01ac538afc23ed4cf
MelanieP31 <132848938+MelanieP31@users.noreply.github.com>
2023-07-31 17:02:21 +0200

    search by category
    version 1 : hardcoded category

--- frontend/angular_ecommerce/src/app/components/product-list/product-list.component.ts
+++ frontend/angular_ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -10,14 +11,37 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductListComponent implements OnInit {
 
   products : Product[] = [];
-  constructor(private productSevice : ProductService ) { }
+  currentCategoryId : number = 1;
+
+
+
+  constructor(private productSevice : ProductService,
+              private route : ActivatedRoute ) { }
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    }); 
+    
   }
 
   listProducts() {
-    this.productSevice.getProductList().subscribe(
+
+    //check if "id" parameter is avaible
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if(hasCategoryId){
+      //get the id param string. convert string to a number + symbole
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    }
+    else{
+      //not cat avaible
+      this.currentCategoryId =1;
+    }
+
+    //now get the products for the given category id
+
+    this.productSevice.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
