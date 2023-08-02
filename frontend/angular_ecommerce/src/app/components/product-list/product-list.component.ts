b15df154bc382e26045097c3eeb17ee6cfd1fc12
MelanieP31 <132848938+MelanieP31@users.noreply.github.com>
2023-08-02 10:29:51 +0200

    add categoryName 'dynamique'

--- frontend/angular_ecommerce/src/app/components/product-list/product-list.component.ts
+++ frontend/angular_ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'src/app/common/product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
@@ -10,42 +10,43 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class ProductListComponent implements OnInit {
 
-  products : Product[] = [];
-  currentCategoryId : number = 1;
+  products: Product[] = [];
+  currentCategoryId: number = 1;
+  currentCategoryName: string = "";
 
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
 
-
-  constructor(private productSevice : ProductService,
-              private route : ActivatedRoute ) { }
-
-  ngOnInit(): void {
+  ngOnInit() {
     this.route.paramMap.subscribe(() => {
       this.listProducts();
-    }); 
-    
+    });
   }
 
   listProducts() {
 
-    //check if "id" parameter is avaible
+    // check if "id" parameter is available
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
 
-    if(hasCategoryId){
-      //get the id param string. convert string to a number + symbole
+    if (hasCategoryId) {
+      // get the "id" param string. convert string to a number using the "+" symbol
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+
+      // get the "name" param string
+      this.currentCategoryName = this.route.snapshot.paramMap.get('name')!;
     }
-    else{
-      //not cat avaible
-      this.currentCategoryId =1;
+    else {
+      // not category id available ... default to category id 1
+      this.currentCategoryId = 1;
+      this.currentCategoryName = 'Books';
     }
 
-    //now get the products for the given category id
-
-    this.productSevice.getProductList(this.currentCategoryId).subscribe(
+    // now get the products for the given category id
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
-    );
+    )
   }
 
 }
