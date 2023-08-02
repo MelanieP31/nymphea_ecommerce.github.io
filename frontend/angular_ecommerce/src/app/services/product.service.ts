420eb18bb4dc9876acab24cde9cd71307caf65ab
MelanieP31 <132848938+MelanieP31@users.noreply.github.com>
2023-08-02 16:39:46 +0200

    search by keyword

--- frontend/angular_ecommerce/src/app/services/product.service.ts
+++ frontend/angular_ecommerce/src/app/services/product.service.ts
@@ -9,6 +9,7 @@ import { ProductCategory } from '../common/product-category';
   providedIn: 'root'
 })
 export class ProductService {
+  
 
   private baseUrl = 'http://localhost:8080/api/products';
 
@@ -20,11 +21,23 @@ export class ProductService {
 
     // need to build URL based on category id 
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+    
+    return this.getProducts(searchUrl);
+ 
+  }
 
+  searchProducts(theKeyword: string) : Observable<Product[]>{
+    const searchUrl =`${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
+
+    return this.getProducts(searchUrl);
+  }
+
+  private getProducts(searchUrl: string): Observable<Product[]> {
     return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
+
 
   getProductCategories(): Observable<ProductCategory[]> {
 
@@ -32,6 +45,9 @@ export class ProductService {
       map(response => response._embedded.productCategory)
     );
   }
+
+ 
+
 
 }
 
