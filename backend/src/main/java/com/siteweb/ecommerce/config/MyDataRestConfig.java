dc79347179626119e2dca5b65e367f7218e6efe1
MelanieP31 <melanie.picot01@gmail.com>
2023-11-24 12:40:40 +0100

    changement ordi

--- backend/src/main/java/com/siteweb/ecommerce/config/MyDataRestConfig.java
+++ backend/src/main/java/com/siteweb/ecommerce/config/MyDataRestConfig.java
@@ -9,8 +9,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-
 import com.siteweb.ecommerce.entity.Product;
 import com.siteweb.ecommerce.entity.ProductCategory;
 import com.siteweb.ecommerce.entity.Country;
--- backend/src/main/java/com/siteweb/ecommerce/dto/PurchaseResponse.java
+++ backend/src/main/java/com/siteweb/ecommerce/dto/PurchaseResponse.java
@@ -8,3 +8,4 @@ public class PurchaseResponse {
     private final String orderTrackingNumber;
 
 }
+ 
